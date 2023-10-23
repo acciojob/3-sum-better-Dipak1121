1d76ceb2824063ee48a9af79a6b91eafbab8f5ab
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-10-23 12:12:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function threeSum(arr, target) {
 // write your code here
-  
+  let ans = 0;
+	let minDiff = Infinity;
+	for ( let i = 0; i < arr.length; i++ ){
+		for ( let j = i+1; j < arr.length; j++ ){
+			for ( let k = j+1; k < arr.length; k++ ){
+				let sum = arr[i] + arr[j] + arr[k];
+				let diff = Math.abs(target - sum);
+				if ( diff < minDiff ){
+					minDiff = diff;
+					ans = sum;
+				}
+			}
+		}
+	}
+	return ans;
 }
 
 module.exports = threeSum;
